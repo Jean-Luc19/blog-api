65e90874b616cd6c5cdefa3a0fc736c7106f8cc5
Terry Hayes <terrychayes@gmail.com>
2017-02-17 15:23:38 -0500

    Adds delete method.

--- postRouter.js
+++ postRouter.js
@@ -23,10 +23,17 @@ router.post('/', jsonParser, (req, res) => {
       console.error(message);
       return res.status(400).send(message);
     };
-    console.log(req.body);
     const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
     return res.status(201).json(item);
   }
+});
+
+router.delete('/:id', (req, res) => {
+  if (!BlogPosts.get(req.params.id)) {
+    return res.sendStatus(404);
+  }
+  BlogPosts.delete(req.params.id);
+  return res.sendStatus(200);
 });
 
 module.exports = router;
