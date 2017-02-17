45869b12ec6c06b952718e737ab66f6f7b6dd3cb
Jean-Luc19 <aticus19@gmail.com>
2017-02-17 12:20:01 -0700

    get method working in postRouter.js

--- server.js
+++ server.js
@@ -2,3 +2,10 @@
 const express = require ('express');
 const bodyParser = require('body-parser');
 const app = express();
+const postRouter = require('./postRouter');
+
+app.use('/blog-posts', postRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
