049f4fc5c178af1ff96d2e4d3302be9050bd2295
Jean-Luc19 <aticus19@gmail.com>
2017-02-21 20:55:39 -0700

    get tests functional

--- test/test-server.js
+++ test/test-server.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
-const {app, runServer, clossServer} = require('../server');
+const {app, runServer, closeServer} = require('../server');
 // this lets us use *should* style syntax in our tests
 // so we can do things like `(1 + 1).should.equal(2);`
 // http://chaijs.com/api/bdd/
@@ -12,4 +12,38 @@ const should = chai.should();
 // see: https://github.com/chaijs/chai-http
 chai.use(chaiHttp);
 
-describe()
+describe('Suite of test for the post API layer', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  it('get should return a list of posts', function() {
+    chai.request(app)
+    .get('/blog-posts')
+    .then(function(res) {
+      res.body.should.be.a('array');
+      res.body[0].title.should.be.equal('my first post');
+      res.body.length.should.be.equal(2);
+    });
+  });
+  it('should check all the content for posts', function() {
+    chai.request(app)
+    .get('/blog-posts')
+    .then(function(res) {
+      const myID = res.body[0].id;
+      chai.request(app)
+      .get(`/blog-posts/${myID}`)
+      .then(function(res) {
+        res.body.should.be.a('object');
+        res.body.title.should.be.a('string');
+        res.body.content.should.be.a('string');
+        res.body.author.should.be.a('string');
+      })
+    });
+  });
+})
